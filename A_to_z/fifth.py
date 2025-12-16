1e653d861023bb97bb2d75ff8a6b49114bbc5144
Co786-stars <raditya82453@gmail.com>
2025-12-16 12:28:28 +0530

    simple rough of decorators today in module A_to_z/fifth.py

---
+++ A_to_z/fifth.py
@@ -0,0 +1,36 @@
+"""def x(a):
+    a()
+
+def fun():
+    print("wizard")
+
+obj = x(fun)
+"""
+
+
+"""def xyz(arg):
+    def pqr():
+        var = arg()
+        print(var)
+    return pqr
+
+def abc():
+    return f"i have no words to explain any {1} line about u"
+xyz(abc)()
+# - A decorator in Python is simply a function that takes another function as input and
+# returns a new function (usually wrapping or modifying behavior).
+"""
+
+"""# another version : -
+def xyz(arg):
+    def pqr():
+        var = arg(2, 3)
+        print(var),
+    print("hello")
+    return pqr
+
+def abc(a, b):
+    return (a+b)
+xyz(abc)()  # basic concept first function is execute then the second one is executed
+"""
+
