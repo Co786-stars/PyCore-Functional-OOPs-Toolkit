d35e36ed780b186cd06272cedb24a2674596e381
Co786-stars <raditya82453@gmail.com>
2025-12-22 11:38:29 +0530

    update some important command related to pip package manager

--- Modules/Module Django/_2_Djangobase_code_.py
+++ Modules/Module Django/_2_Djangobase_code_.py
@@ -1,5 +1,19 @@
 """
 Basic cmd commands that use to check Django is installed or not ?
+> python -m pip install -upgrade pip
+  by using this command we update the pip download package manager of python
+
+> pip --version
+  to check the version of pip
+
+
+> python -m venv .venv
+  To make sure our virtual environment is properly created.
+
+> pip install -upgrade pip setuptools wheel
+  To Reinstall standard libraries if needed
+
+
 > pip show django
   by using this we see the version, location, summary, homepage... etc. about django if it is installed in system.
 
@@ -27,5 +41,22 @@ Basic cmd commands that use to check Django is installed or not ?
   because give access to normal the Script folder.
 
 > django-admin startproject myproject
-  # need to read about this command ..
+  creates a new Django project with default settings and structure
+  simple steps that we follow to execute this command in cmd : -
+  create a new  directory  and open in cmd and use this command.
+  suppose we create directory "xyz" then  C:\Users\<YourName>\xyz>django-admin startproject MyprojectName
+
 """
+
+
+# Quick summary : -
+# python -m pip install --upgrade pip        → updates pip to the latest version
+# pip --version                              → checks the installed pip version
+# python -m venv .venv                       → creates a virtual environment named .venv
+# pip install --upgrade pip setuptools wheel → upgrades/reinstalls essential Python packaging tools
+# pip show django                            → displays details if Django is installed (version, location, etc.)
+# where python                               → shows the exact location of the Python executable
+# pip install django                         → installs the Django framework
+# django-admin startproject myproject        → creates a new Django project with default settings and structure
+
+
