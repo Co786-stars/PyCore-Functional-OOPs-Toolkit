d40239ef3f13e93850fd0a94335d71cd26d504a4
Co786-stars <raditya82453@gmail.com>
2026-02-26 17:40:23 +0530

    hackerRank anothor way

---
+++ All In Questions/Q_Hacker Rank/Python_01 HelloWorld _ HackerRank.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    simple_var = "Hello, World!"
+    print(simple_var)
+
+# another method to display th string very easily is
+if __name__ == '__main__':
+    print("Hello, World!")
+
+
+# Question : WAP a program to display the "Hello, World".
